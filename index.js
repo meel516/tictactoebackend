f76ccca51cbabf478e8574a804504c2f09e8fa9c
meel516 <md.saleem516@gmail.com>
2023-08-09 01:15:02 +0530

    added multiple users api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require('express')
 let {chatDb}=require('./connector')
 let bodyParser=require('body-parser')
+let url=require('url')
 const cors=require('cors') 
 let saleem="my first message"
 let samplemessage="hi iam saleem"
@@ -9,21 +10,20 @@ let app= express()
 app.use(cors())
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false}));
 app.listen(5000,()=>console.log('listening on port 3000'))
 // app.use('/',(req,res)=>res.end('hi saleem'))
 app.get('/getMessage',(req,res)=>{
     async function get(){
-        const messages= await chatDb.find({})
-        console.log(messages,'hi messages')
-        res.send(messages)
+        const messages= await chatDb.find({username:req.query.username,opposite:req.query.opposite})
+      res.send(messages)
     }
     get()
    
 })
 app.post('/newMessage', (req,res)=>{
     async function sendMessage(){
-let  newMessage = new chatDb({username:req.body.username,message:req.body.message,time:Date.now()})
+let  newMessage = new chatDb({username:req.body.username,opposite:req.body.opposite,message:req.body.message,time:Date.now()})
 const messageUpload =await newMessage.save()
 res.send(messageUpload)}
 sendMessage() 
--- model/chat.js
+++ model/chat.js
@@ -1,6 +1,7 @@
 const { Schema, mongo } = require('mongoose');
 const chatSchema = new Schema({
     username: Schema.Types.String,
+    opposite:Schema.Types.String,
     message: Schema.Types.String,
     time: Schema.Types.String
 })
